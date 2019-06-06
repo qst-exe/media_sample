98470b8cfd1313086eecbf347d7fd6c7a2909345
Task Nagashige <task.19.xx@gmail.com>
2019-06-06 19:50:29 +0900

    routerの重複を消した

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  resources :users
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
 end
