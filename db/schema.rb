60e5eaa08237708d50ebac410ad8bb1ab6bd504c
Task Nagashige <task.19.xx@gmail.com>
2019-06-06 18:02:13 +0900

    ログイン機能の発展版の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190605185124) do
+ActiveRecord::Schema.define(version: 20190606084737) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190605185124) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
